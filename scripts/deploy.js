124ce6d7d969dbb142b7cbb71e33070f27d57324
sahana-9314 <87483495+sahana-9314@users.noreply.github.com>
2023-07-16 14:40:48 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,62 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const ethers = require('ethers');
+const contractABI = require('./contractABI.json');
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+// Connect to the Ethereum network
+const provider = new ethers.providers.JsonRpcProvider('<YOUR_ETHEREUM_RPC_URL>');
+const privateKey = '<YOUR_PRIVATE_KEY>';
+const wallet = new ethers.Wallet(privateKey, provider);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+// Create a contract instance
+const contractAddress = '<YOUR_CONTRACT_ADDRESS>';
+const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
-  await lock.waitForDeployment();
+// Example function to create a new booking
+async function createBooking(bookingId, sexWorker, startTime, endTime) {
+  const overrides = {
+    value: ethers.utils.parseEther('<BOOKING_VALUE_ETH>'), // Set the booking value in Ether
+  };
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const tx = await contract.createBooking(bookingId, sexWorker, startTime, endTime, overrides);
+  await tx.wait();
+
+  console.log('Booking created successfully!');
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Example function to complete a booking
+async function completeBooking(bookingId) {
+  const tx = await contract.completeBooking(bookingId);
+  await tx.wait();
+
+  console.log('Booking completed successfully!');
+}
+
+// Example function to get the details of a booking
+async function getBooking(bookingId) {
+  const booking = await contract.getBooking(bookingId);
+
+  console.log('Booking details:');
+  console.log('Customer:', booking.customer);
+  console.log('Sex Worker:', booking.sexWorker);
+  console.log('Start Time:', new Date(booking.startTime * 1000));
+  console.log('End Time:', new Date(booking.endTime * 1000));
+  console.log('Completed:', booking.completed);
+}
+
+// Run the example functions
+async function testContract() {
+  try {
+    // Create a booking
+    await createBooking(1, '<SEX_WORKER_ADDRESS>', <START_TIME>, <END_TIME>);
+
+    // Complete the booking
+    await completeBooking(1);
+
+    // Get the booking details
+    await getBooking(1);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+testContract();
